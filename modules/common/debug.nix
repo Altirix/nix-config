cb830edbc9db0a9eea095efda2be1c73e0342897
Altirix <19439141+Altirix@users.noreply.github.com>
2026-09-07 12:16:23 +0100

    enable ssh

--- modules/common/debug.nix
+++ modules/common/debug.nix
@@ -6,6 +6,12 @@
     hashedPassword = "$6$7d8IZnWOR/EN5GMv$qrOdMoX9ZM4MdEt.Ab4qDfj.ea.cmf4..Lidif2aFkD.cc4Q4B4DHQH9J/2L1b.G28cO7JVoPxk2uQDN.ISZN0";
   };
 
-  # allow over SSH too
-  services.openssh.settings.PermitRootLogin = pkgs.lib.mkForce "yes";
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+      PasswordAuthentication = true;
+    };
+  };
+
 }
