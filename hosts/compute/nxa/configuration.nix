539d2c73c2a7fd34948b7d9a5a33d26d29ea993d
Altirix <19439141+Altirix@users.noreply.github.com>
2026-09-10 01:30:50 +0100

    setup sops secrets

--- hosts/compute/nxa/configuration.nix
+++ hosts/compute/nxa/configuration.nix
@@ -8,4 +8,6 @@
   networking.hostId = "8425e349"; # unique id `head -c4 /dev/urandom | od -A none -t x4 | tr -d ' '`
 
   system.stateVersion = "25.11";
+
+  sops.defaultSopsFile = ./secrets.yaml;
 }
